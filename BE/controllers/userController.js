a81a5443f6b180dd78b467c080ef5204c8d4bd6a
maidzui123 <maiduy190802@gmail.com>
2024-03-23 13:15:38 +0700

    Add OTP functionality for password reset

--- BE/controllers/userController.js
+++ BE/controllers/userController.js
@@ -1,8 +1,11 @@
 import bcrypt from "bcryptjs";
 import UserSchema from "../models/UserSchema.js";
+import OTPSchema from "../models/OTPSchema.js";
 const userController = {};
 import jwt from "jsonwebtoken";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+import e from "express";
 dotenv.config();
 // def get_hashed_password(password: str) -> str:
 //     return pwd_context.hash(password)
@@ -22,8 +25,10 @@ const generate_access_token = (tokenData, JWTSecret_Key, JWT_EXPIRE) => {
 
 userController.register = async (req, res, next) => {
   try {
-    if (!req.body.username || !req.body.email || !req.body.phone) {
-      return res.status(400).send("Username, email, and phone are required.");
+    if (!req.body.username || !req.body.email || !req.body.password) {
+      return res
+        .status(400)
+        .send("Username, email, and password are required.");
     }
 
     const user_in_db = await UserSchema.findOne({ email: req.body.email });
@@ -35,7 +40,6 @@ userController.register = async (req, res, next) => {
     const newUser = new UserSchema({
       username: req.body.username,
       email: req.body.email,
-      phone: req.body.phone,
       password: get_hashed_password(req.body.password),
     });
 
@@ -59,7 +63,10 @@ userController.login = async (req, res, next) => {
     if (!user_in_db) {
       return res.status(400).json({ error: "User does not exist" });
     }
-    const isPasswordValid = await verify_password(password, user_in_db.password);
+    const isPasswordValid = await verify_password(
+      password,
+      user_in_db.password
+    );
     if (!isPasswordValid) {
       return res.status(400).json({ error: "Password is not correct" });
     }
@@ -76,6 +83,114 @@ userController.login = async (req, res, next) => {
     return res
       .status(200)
       .json({ status: true, success: "sendData", token: token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+userController.createOTP = async (req, res, next) => {
+  const { email } = req.body;
+  try {
+    const user = await UserSchema.findOne({ email });
+    const expireOTP = new Date();
+    expireOTP.setMinutes(expireOTP.getMinutes() + 5);
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    let digits = "0123456789";
+    let OTP = "";
+    for (let i = 0; i < 6; i++) {
+      OTP += digits[Math.floor(Math.random() * 10)];
+    }
+    const newOTP = new OTPSchema({ userId: user._id, otp: OTP });
+    await newOTP.save();
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+    var mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: "Reset Password",
+      text: `Your OTP verification is: ${OTP}`,
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+        return res.status(500).send("Error sending email");
+      } else {
+        return res.send({
+          status: true,
+          success: "sendData",
+          user_id: user._id,
+          otp: OTP,
+        });
+      }
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+userController.verifyOTP = async (req, res, next) => {
+  const { otp } = req.body;
+  const userId = req.params.id;
+  try {
+    if (!otp) {
+      return res.status(400).send("OTP is required");
+    }
+    const otp_in_db = await OTPSchema.findOne({ userId, otp });
+    if (!otp_in_db) {
+      return res.status(400).send("OTP is not correct");
+    }
+    if (otp_in_db.expiresAt < new Date()) {
+      return res.status(400).send("OTP has expired");
+    }
+    return res.send({
+      status: true,
+      success: "sendData",
+      message: "OTP is correct",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+userController.resetPassword = async (req, res, next) => {
+  const userId = req.params.id;
+  const { password } = req.body;
+  try {
+    if (!password) {
+      return res.status(400).send("Password is required");
+    }
+    const user = await UserSchema.findById(userId);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    user.password = get_hashed_password(password);
+    await user.save();
+    return res.send({
+      status: true,
+      success: "sendData",
+      message: "Password reset successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+userController.me = async (req, res, next) => {
+  try {
+    const user = await UserSchema.findById(req.user._id).select(
+      "id email username"
+    );
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    return res.send(user);
   } catch (error) {
     next(error);
   }
@@ -114,16 +229,4 @@ userController.login = async (req, res, next) => {
 //     next(err);
 //   }
 // };
-
-userController.me = async (req, res, next) => {
-  try {
-    const user = await UserSchema.findById(req.user._id).select("id email username phone");
-    if (!user) {
-      return res.status(404).send("User not found");
-    }
-    return res.send(user);
-  } catch (error) {
-    next(error);
-  }
-};
 export default userController;
--- BE/routes/userRoutes.js
+++ BE/routes/userRoutes.js
@@ -9,6 +9,11 @@ router.post("/api/user/register", userController.register);
 router.post("/api/user/login", userController.login);
 // me
 router.get("/api/user/me", authentication, userController.me);
-
+// create OTP
+router.post("/api/user/otp", userController.createOTP);
+// verify OTP
+router.post("/api/user/verify-otp/:id", userController.verifyOTP);
+// reset password
+router.post("/api/user/reset-password/:id", userController.resetPassword);
 
 export default router;

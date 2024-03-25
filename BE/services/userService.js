fcc9baf9922445b62e47a0121a813bd0b8494513
maidzui123 <maiduy190802@gmail.com>
2024-03-25 09:18:30 +0700

    Fix bug in login functionality

--- BE/services/userService.js
+++ BE/services/userService.js
@@ -1,4 +1,12 @@
-export const generateHtmlWelcome = (username, otp) => {
+import bcrypt from "bcryptjs";
+import UserSchema from "../models/UserSchema.js";
+import OTPSchema from "../models/OTPSchema.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+dotenv.config();
+
+const generateHtmlWelcome = (username, otp) => {
   return `
       <html lang="en">
       <head>
@@ -236,8 +244,8 @@ export const generateHtmlWelcome = (username, otp) => {
     `;
 };
 
-export const generateHtmlRenew = (username, otp) => {
-    return `
+const generateHtmlRenew = (username, otp) => {
+  return `
         <html lang="en">
         <head>
           <meta charset="UTF-8" />
@@ -463,9 +471,9 @@ export const generateHtmlRenew = (username, otp) => {
         </body>
       </html>
       `;
-  };
+};
 
-export const generateHtmlReset = (username, otp) => {
+const generateHtmlReset = (username, otp) => {
   return `
     <html lang="en">
     <head>
@@ -689,4 +697,309 @@ export const generateHtmlReset = (username, otp) => {
     </body>
   </html>
     `;
+};
+
+const get_hashed_password = (password) => {
+  return bcrypt.hashSync(password, 8);
+};
+
+const verify_password = (plain_password, hashed_password) => {
+  return bcrypt.compare(plain_password, hashed_password);
+};
+
+const generate_access_token = (tokenData, JWTSecret_Key, JWT_EXPIRE) => {
+  return jwt.sign(tokenData, JWTSecret_Key, { expiresIn: JWT_EXPIRE });
+};
+
+const create_otp = async () => {
+  let digits = "0123456789";
+  let OTP = "";
+  for (let i = 0; i < 6; i++) {
+    OTP += digits[Math.floor(Math.random() * 10)];
+  }
+  return OTP;
+};
+
+const send_email_reset = async (res, user, otp) => {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+  var mailOptions = {
+    from: process.env.EMAIL,
+    to: user.email,
+    subject: "Reset Password",
+    html: generateHtmlReset(user.username, otp),
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      return res.status(500).send("Error sending email");
+    }
+  });
+  return res.status(200).send({
+    status: true,
+    success: "Add OTP to reset password",
+    user_id: user._id,
+  });
+};
+const send_email_register = async (res, user, otp) => {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+  var mailOptions = {
+    from: process.env.EMAIL,
+    to: user.email,
+    subject: "Welcome to WordWizard!",
+    html: generateHtmlWelcome(user.username, otp),
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      return res.status(500).send("Error sending email");
+    }
+  });
+  return res.status(200).send({
+    status: true,
+    message: "Add OTP to verify your account",
+    user_id: user._id,
+  });
+};
+
+const send_email_renew = async (res, user, otp) => {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+  var mailOptions = {
+    from: process.env.EMAIL,
+    to: user.email,
+    subject: "Here your new OTP!",
+    html: generateHtmlRenew(user.username, otp),
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      return res.status(500).send("Error sending email");
+    }
+  });
+  return res.status(200).send({
+    status: true,
+    message: "Add OTP to verify your account",
+    user_id: user._id,
+  });
+};
+
+const create_token = async (res, user) => {
+  let tokenData;
+  tokenData = { _id: user._id, email: user.email };
+
+  const token = await generate_access_token(
+    tokenData,
+    process.env.JWT_SECRET,
+    process.env.JWT_EXPIRE
+  );
+  return res
+    .status(200)
+    .json({ status: true, success: "Login Success", token: token });
+};
+
+const create_or_update_otp = async (user, otp) => {
+  const expireOTP = new Date();
+  expireOTP.setMinutes(expireOTP.getMinutes() + 1);
+  const checkOTP = await OTPSchema.findOne({ userId: user._id });
+  if (checkOTP) {
+    await OTPSchema.findByIdAndUpdate(checkOTP._id, {
+      otp: otp,
+      expiresAt: expireOTP,
+    });
+  } else {
+    const newOTP = new OTPSchema({
+      userId: user._id,
+      otp: otp,
+      expiresAt: expireOTP,
+    });
+    await newOTP.save();
+  }
+};
+
+const handle_register = async (req, res) => {
+  const user_in_db = await UserSchema.findOne({ email: req.body.email });
+
+  if (user_in_db) {
+    return res
+      .status(400)
+      .json({ errorCode: "2", message: "User already exists" });
+  }
+
+  const newUser = new UserSchema({
+    username: req.body.username,
+    email: req.body.email,
+    password: get_hashed_password(req.body.password),
+  });
+
+  const user = await newUser.save();
+  const OTP = await create_otp();
+  await create_or_update_otp(user, OTP);
+  return await send_email_register(res, user, OTP);
+};
+
+const handle_renew_otp = async (email, res) => {
+  const user = await UserSchema.findOne({ email });
+  if (!user) {
+    return res.status(404).send({
+      errorCode: "2",
+      message: "User not found",
+    });
+  }
+  let OTP = await create_otp();
+  await create_or_update_otp(user, OTP);
+  return await send_email_renew(res, user, OTP);
+};
+
+const handle_login_otp = async (otp, userId, res) => {
+  const user_in_db = await UserSchema.findById(userId);
+  if (!user_in_db) {
+    return res.status(400).json({
+      errorCode: "2",
+      message: "User not found",
+    });
+  }
+  const otp_in_db = await OTPSchema.findOne({ userId: userId, otp });
+  if (!otp_in_db) {
+    return res.status(400).json({
+      errorCode: "3",
+      message: "OTP is wrong or expired",
+    });
+  }
+
+  return await create_token(res, user_in_db);
+};
+
+const handle_login = async (email, password, res) => {
+  const user_in_db = await UserSchema.findOne({ email: email });
+  if (!user_in_db) {
+    return res.status(400).json({
+      errorCode: "2",
+      message: "User not found",
+    });
+  }
+  const isPasswordValid = await verify_password(password, user_in_db.password);
+  if (!isPasswordValid) {
+    return res.status(400).json({
+      errorCode: "3",
+      message: "Password is not correct",
+    });
+  }
+
+  return await create_token(res, user_in_db);
+};
+
+const handle_change_password = async (
+  req,
+  currentPassword,
+  newPassword,
+  res
+) => {
+  const user = await UserSchema.findById(req.user._id);
+  if (!user) {
+    return res.status(404).send({
+      errorCode: "2",
+      message: "User not found",
+    });
+  }
+  const isPasswordValid = await verify_password(currentPassword, user.password);
+  if (!isPasswordValid) {
+    return res.status(400).send({
+      errorCode: "3",
+      message: "Current password is not correct",
+    });
+  }
+  user.password = get_hashed_password(newPassword);
+  await user.save();
+  return res.status(200).send({
+    status: true,
+    success: "sendData",
+    message: "Password changed successfully",
+  });
+};
+
+const handle_create_otp = async (email, res) => {
+  const user = await UserSchema.findOne({ email });
+
+  if (!user) {
+    return res.status(404).send({
+      errorCode: "2",
+
+      message: "User not found",
+    });
+  }
+  let OTP = await create_otp();
+  await create_or_update_otp(user, OTP);
+  return await send_email_reset(res, user, OTP);
+};
+
+const handle_verify_otp = async (otp, userId, res) => {
+  const otp_in_db = await OTPSchema.findOne({ userId, otp });
+  if (!otp_in_db) {
+    return res.status(400).send({
+      errorCode: "2",
+      message: "OTP is wrong or expired",
+    });
+  }
+  return res.status(200).send({
+    status: true,
+    success: "sendData",
+    message: "OTP is correct",
+  });
+};
+
+const handle_reset_password = async (userId, password, res) => {
+  const user = await UserSchema.findById(userId);
+  if (!user) {
+    return res.status(404).send({
+      errorCode: "2",
+      message: "User not found",
+    });
+  }
+  user.password = get_hashed_password(password);
+  await user.save();
+  return res.send({
+    status: true,
+    success: "sendData",
+    message: "Password reset successfully",
+  });
+};
+
+export  {
+    handle_register,
+    handle_login,
+    handle_login_otp,
+    handle_change_password,
+    handle_create_otp,
+    handle_verify_otp,
+    handle_reset_password,
+    handle_renew_otp,
+    generate_access_token,
+    get_hashed_password,
+    verify_password,
+    create_otp,
+    send_email_reset,
+    send_email_register,
+    send_email_renew,
+    create_or_update_otp,
+    create_token,
+    generateHtmlWelcome,
+    generateHtmlRenew,
+    generateHtmlReset,
 };
